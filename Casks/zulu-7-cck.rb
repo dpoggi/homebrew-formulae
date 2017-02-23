f057f9873a581242a9ef256a6ccdc74c08937887
Dan Poggi <dan.poggi@gmail.com>
2017-02-23 17:04:25 -0500

    Add casks: zulu-7-cck, zulu-8-cck

---
+++ Casks/zulu-7-cck.rb
@@ -0,0 +1,30 @@
+cask 'zulu-7-cck' do
+  version '7.0.0.4,7.0_131'
+  sha256 '8602d339b2b2384895d2c56abeab55b02931a7943c4a3a7880b6d770d1b07511'
+
+  url "https://cdn.azul.com/zcck/bin/zcck#{version.before_comma}-macosx_x64.sh",
+      referer: 'https://www.azul.com/products/zulu/cck-downloads/mac-os-x/'
+  name 'Azul Zulu Commercial Compatibility Kit for Java SE 7'
+  homepage 'https://www.azul.com/products/zulu/cck-downloads/'
+
+  depends_on cask: 'zulu-7'
+
+  container type: :naked
+
+  postflight do
+    IO.write staged_path/'install_cck.tcl', <<-EOS.undent
+      set timeout -1
+
+      spawn -noecho /bin/sh #{staged_path}/zcck#{version.before_comma}-macosx_x64.sh
+
+      expect "patch:"
+      send "/Library/Java/JavaVirtualMachines/zulu1.#{version.after_comma}.jdk/Contents/Home\\r"
+
+      expect eof
+    EOS
+
+    system_command '/usr/bin/expect',
+                   args: [staged_path/'install_cck.tcl'],
+                   sudo: true
+  end
+end
---
+++ Casks/zulu-8-cck.rb
@@ -0,0 +1,30 @@
+cask 'zulu-8-cck' do
+  version '8.0.0.4,1.8.0_121'
+  sha256 '9f69fcec07b915ace5d0ccaea164b0a3492ac7dceed7f42224c23098ff0613e0'
+
+  url "https://cdn.azul.com/zcck/bin/zcck#{version.before_comma}-macosx_x64.sh",
+      referer: 'https://www.azul.com/products/zulu/cck-downloads/mac-os-x/'
+  name 'Azul Zulu Commercial Compatibility Kit for Java SE 8'
+  homepage 'https://www.azul.com/products/zulu/cck-downloads/'
+
+  depends_on cask: 'zulu-8'
+
+  container type: :naked
+
+  postflight do
+    IO.write staged_path/'install_cck.tcl', <<-EOS.undent
+      set timeout -1
+
+      spawn -noecho /bin/sh #{staged_path}/zcck#{version.before_comma}-macosx_x64.sh
+
+      expect "patch:"
+      send "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home\\r"
+
+      expect eof
+    EOS
+
+    system_command '/usr/bin/expect',
+                   args: [staged_path/'install_cck.tcl'],
+                   sudo: true
+  end
+end
