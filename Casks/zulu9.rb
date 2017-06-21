69d5970f6043f094a1814d5b1f3d677c0aca7590
Dan Poggi <dan.poggi@gmail.com>
2017-06-21 17:36:15 -0400

    Version bump for Zulu 9.0.0.12

--- Casks/zulu9.rb
+++ Casks/zulu9.rb
@@ -1,21 +1,22 @@
 cask 'zulu9' do
-  version '9.0.0,9.0.0.10'
-  sha256 '3b349acaaba370724f03b382cfe0672c260c71da7ad54f7490a1967e3c07a516'
+  version '9.0.0,9.0.0.12'
+  sha256 '1e8a5ff26a94f768e2f9a6bec83c0185eff3c08c6c3d1fa52262fd89a1b66b97'
 
   url "https://cdn.azul.com/zulu-pre/bin/zulu#{version.after_comma}-ea-jdk#{version.before_comma}-macosx_x64.dmg",
       referer: 'https://zulu.org/zulu-9-pre-release-downloads/'
-  name 'Azul Zulu Java SE 8 Development Kit'
+  name 'Azul Zulu Java SE 9 Development Kit'
   homepage 'https://zulu.org/zulu-9-pre-release-downloads/'
 
   pkg "Double-Click to Install Zulu #{version.major}.pkg"
 
   postflight do
-    system_command '/bin/mv',
-                   args: ['-f', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk"],
-                   sudo: true
+    # TODO: Use after Java 9 goes final
+    # system_command '/bin/mv',
+    #                args: ['-f', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk"],
+    #                sudo: true
 
     system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk/Contents/Info.plist"],
+                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk/Contents/Info.plist"],
                    sudo: true
   end
 
@@ -23,6 +24,6 @@ cask 'zulu9' do
                        "com.azulsystems.zulu.#{version.major}",
                      ],
             delete:  [
-                       "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk",
+                       "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk",
                      ].keep_if { |v| !v.nil? }
 end
