94d56a0b35535a182a4793ff5ea20ef1db6eb833
Dan Poggi <dan.poggi@gmail.com>
2017-07-28 17:08:59 -0400

    Update zulu@9 to 9.0.0,9.0.0.13

--- Casks/zulu@9.rb
+++ Casks/zulu@9.rb
@@ -1,6 +1,6 @@
-cask 'zulu9' do
-  version '9.0.0,9.0.0.12'
-  sha256 '1e8a5ff26a94f768e2f9a6bec83c0185eff3c08c6c3d1fa52262fd89a1b66b97'
+cask 'zulu@9' do
+  version '9.0.0,9.0.0.13'
+  sha256 'edfe190664bf8e588cf92925f612ef78d9915e5470c824c1296acb5a72f7654b'
 
   url "https://cdn.azul.com/zulu-pre/bin/zulu#{version.after_comma}-ea-jdk#{version.before_comma}-macosx_x64.dmg",
       referer: 'https://zulu.org/zulu-9-pre-release-downloads/'
@@ -10,13 +10,8 @@ cask 'zulu9' do
   pkg "Double-Click to Install Zulu #{version.major}.pkg"
 
   postflight do
-    # TODO: Use after Java 9 goes final
-    # system_command '/bin/mv',
-    #                args: ['-f', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk"],
-    #                sudo: true
-
     system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk/Contents/Info.plist"],
+                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk/Contents/Info.plist"],
                    sudo: true
   end
 
@@ -25,5 +20,5 @@ cask 'zulu9' do
                      ],
             delete:  [
                        "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk",
-                     ].keep_if { |v| !v.nil? }
+                     ]
 end
