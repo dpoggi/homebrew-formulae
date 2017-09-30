9cad9329b343f7e102efa098b832bc183e6ff7e3
Dan Poggi <dan.poggi@gmail.com>
2017-09-30 19:12:47 -0400

    Line up zulu-cck versioning with zulu casks

--- Casks/zulu-cck@7.rb
+++ Casks/zulu-cck@7.rb
@@ -1,9 +1,9 @@
 cask 'zulu-cck@7' do
-  version '7.0.0.4,1.7.0_154'
+  version '1.7.0_154,7.0.0.4'
   sha256 '8602d339b2b2384895d2c56abeab55b02931a7943c4a3a7880b6d770d1b07511'
 
   # cdn.azul.com was verified as official when first introduced to the cask
-  url "https://cdn.azul.com/zcck/bin/zcck#{version.before_comma}-macosx_x64.sh",
+  url "https://cdn.azul.com/zcck/bin/zcck#{version.after_comma}-macosx_x64.sh",
       referer: 'https://www.azul.com/products/zulu-and-zulu-enterprise/cck-downloads/mac-os-x/'
   name 'Commercial Compatibility Kit for Zulu 7'
   homepage 'https://zulu.org/developer-resources/commercial-compatibility/'
@@ -13,23 +13,23 @@ cask 'zulu-cck@7' do
 
   postflight do
     system_command '/bin/bash',
-                   args: ['-c', "tail -n \"+$(awk '/^__ARCHIVE_START__/ { print NR + 1; exit 0; }' \"#{staged_path.join("zcck#{version.before_comma}-macosx_x64.sh")}\")\" \"#{staged_path.join("zcck#{version.before_comma}-macosx_x64.sh")}\" | tar xf - -C \"#{staged_path}\""]
+                   args: ['-c', "tail -n \"+$(awk '/^__ARCHIVE_START__/ { print NR + 1; exit 0; }' \"#{staged_path.join("zcck#{version.after_comma}-macosx_x64.sh")}\")\" \"#{staged_path.join("zcck#{version.after_comma}-macosx_x64.sh")}\" | tar xf - -C \"#{staged_path}\""]
 
     system_command '/bin/mkdir',
-                   args: ['-p', "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/etc"],
+                   args: ['-p', "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/etc"],
                    sudo: true
     Dir.glob(staged_path.join('license', '*')) do |license|
       system_command '/bin/ln',
-                     args: ['-nsf', license, "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/etc/#{File.basename(license)}"],
+                     args: ['-nsf', license, "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/etc/#{File.basename(license)}"],
                      sudo: true
     end
 
     system_command '/bin/mkdir',
-                   args: ['-p', "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/jre/lib/fonts"],
+                   args: ['-p', "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/jre/lib/fonts"],
                    sudo: true
     Dir.glob(staged_path.join('fonts', '*')) do |font|
       system_command '/bin/ln',
-                     args: ['-nsf', font, "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/jre/lib/fonts/#{File.basename(font)}"],
+                     args: ['-nsf', font, "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/jre/lib/fonts/#{File.basename(font)}"],
                      sudo: true
     end
   end
@@ -37,20 +37,20 @@ cask 'zulu-cck@7' do
   uninstall_postflight do
     Dir.glob(staged_path.join('license', '*')) do |license|
       system_command '/bin/rm',
-                     args: ['-f', "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/etc/#{File.basename(license)}"],
+                     args: ['-f', "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/etc/#{File.basename(license)}"],
                      sudo: true
     end
     system_command '/bin/bash',
-                   args: ['-c', "rmdir /Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/etc >/dev/null 2>&1 || true"],
+                   args: ['-c', "rmdir /Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/etc >/dev/null 2>&1 || true"],
                    sudo: true
 
     Dir.glob(staged_path.join('fonts', '*')) do |font|
       system_command '/bin/rm',
-                     args: ['-f', "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/jre/lib/fonts/#{File.basename(font)}"],
+                     args: ['-f', "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/jre/lib/fonts/#{File.basename(font)}"],
                      sudo: true
     end
     system_command '/bin/bash',
-                   args: ['-c', "rmdir /Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/jre/lib/fonts >/dev/null 2>&1 || true"],
+                   args: ['-c', "rmdir /Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/jre/lib/fonts >/dev/null 2>&1 || true"],
                    sudo: true
   end
 
--- Casks/zulu-cck@8.rb
+++ Casks/zulu-cck@8.rb
@@ -1,9 +1,9 @@
 cask 'zulu-cck@8' do
-  version '8.0.0.4,1.8.0_144'
+  version '1.8.0_144,8.0.0.4'
   sha256 '9f69fcec07b915ace5d0ccaea164b0a3492ac7dceed7f42224c23098ff0613e0'
 
   # cdn.azul.com was verified as official when first introduced to the cask
-  url "https://cdn.azul.com/zcck/bin/zcck#{version.before_comma}-macosx_x64.sh",
+  url "https://cdn.azul.com/zcck/bin/zcck#{version.after_comma}-macosx_x64.sh",
       referer: 'https://www.azul.com/products/zulu-and-zulu-enterprise/cck-downloads/mac-os-x/'
   name 'Commercial Compatibility Kit for Zulu 8'
   homepage 'https://zulu.org/developer-resources/commercial-compatibility/'
@@ -13,23 +13,23 @@ cask 'zulu-cck@8' do
 
   postflight do
     system_command '/bin/bash',
-                   args: ['-c', "tail -n \"+$(awk '/^__ARCHIVE_START__/ { print NR + 1; exit 0; }' \"#{staged_path.join("zcck#{version.before_comma}-macosx_x64.sh")}\")\" \"#{staged_path.join("zcck#{version.before_comma}-macosx_x64.sh")}\" | tar xf - -C \"#{staged_path}\""]
+                   args: ['-c', "tail -n \"+$(awk '/^__ARCHIVE_START__/ { print NR + 1; exit 0; }' \"#{staged_path.join("zcck#{version.after_comma}-macosx_x64.sh")}\")\" \"#{staged_path.join("zcck#{version.after_comma}-macosx_x64.sh")}\" | tar xf - -C \"#{staged_path}\""]
 
     system_command '/bin/mkdir',
-                   args: ['-p', "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/etc"],
+                   args: ['-p', "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/etc"],
                    sudo: true
     Dir.glob(staged_path.join('license', '*')) do |license|
       system_command '/bin/ln',
-                     args: ['-nsf', license, "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/etc/#{File.basename(license)}"],
+                     args: ['-nsf', license, "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/etc/#{File.basename(license)}"],
                      sudo: true
     end
 
     system_command '/bin/mkdir',
-                   args: ['-p', "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/jre/lib/fonts"],
+                   args: ['-p', "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/jre/lib/fonts"],
                    sudo: true
     Dir.glob(staged_path.join('fonts', '*')) do |font|
       system_command '/bin/ln',
-                     args: ['-nsf', font, "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/jre/lib/fonts/#{File.basename(font)}"],
+                     args: ['-nsf', font, "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/jre/lib/fonts/#{File.basename(font)}"],
                      sudo: true
     end
   end
@@ -37,20 +37,20 @@ cask 'zulu-cck@8' do
   uninstall_postflight do
     Dir.glob(staged_path.join('license', '*')) do |license|
       system_command '/bin/rm',
-                     args: ['-f', "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/etc/#{File.basename(license)}"],
+                     args: ['-f', "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/etc/#{File.basename(license)}"],
                      sudo: true
     end
     system_command '/bin/bash',
-                   args: ['-c', "rmdir /Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/etc >/dev/null 2>&1 || true"],
+                   args: ['-c', "rmdir /Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/etc >/dev/null 2>&1 || true"],
                    sudo: true
 
     Dir.glob(staged_path.join('fonts', '*')) do |font|
       system_command '/bin/rm',
-                     args: ['-f', "/Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/jre/lib/fonts/#{File.basename(font)}"],
+                     args: ['-f', "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/jre/lib/fonts/#{File.basename(font)}"],
                      sudo: true
     end
     system_command '/bin/bash',
-                   args: ['-c', "rmdir /Library/Java/JavaVirtualMachines/zulu#{version.after_comma}.jdk/Contents/Home/jre/lib/fonts >/dev/null 2>&1 || true"],
+                   args: ['-c', "rmdir /Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Home/jre/lib/fonts >/dev/null 2>&1 || true"],
                    sudo: true
   end
 
