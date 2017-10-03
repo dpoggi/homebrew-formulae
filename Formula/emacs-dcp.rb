b830a325ccd8b5c4559f5406d6b9c4a58e6be342
Dan Poggi <dan.poggi@gmail.com>
2017-10-03 19:30:44 -0400

    emacs-dcp now always pulls from emacs-26

--- Formula/emacs-dcp.rb
+++ Formula/emacs-dcp.rb
@@ -1,18 +1,8 @@
 class EmacsDcp < Formula
   desc "GNU Emacs text editor"
   homepage "https://www.gnu.org/software/emacs/"
-  url "https://ftp.gnu.org/gnu/emacs/emacs-25.3.tar.xz"
-  mirror "https://ftpmirror.gnu.org/emacs/emacs-25.3.tar.xz"
-  sha256 "253ac5e7075e594549b83fd9ec116a9dc37294d415e2f21f8ee109829307c00b"
-
-  head do
-    url "https://github.com/emacs-mirror/emacs.git", :branch => "emacs-26"
-
-    depends_on "autoconf" => :build
-    depends_on "gnu-sed" => :build
-    depends_on "little-cms2"
-    depends_on "texinfo" => :build
-  end
+  url "https://github.com/emacs-mirror/emacs.git", :branch => "emacs-26"
+  version "26.0.60"
 
   option "without-cocoa",
          "Build a non-Cocoa version of Emacs"
@@ -20,28 +10,21 @@ class EmacsDcp < Formula
          "Build without dynamic modules support"
   option "without-spacemacs-icon",
          "Build without Spacemacs icon by Nasser Alshammari"
-  option "with-x11",
-         "Experimental: build with x11 support"
   option "with-natural-title-bar",
-         "Experimental: use a title bar colour inferred by your theme"
+         "Experimental: use a title bar color inferred from your theme"
 
-  deprecated_option "cocoa" => "with-cocoa"
-  deprecated_option "with-d-bus" => "with-dbus"
-
-  depends_on "pkg-config" => :build
-  depends_on :x11 => :optional
-  depends_on "dbus" => :optional
+  depends_on "autoconf" => :build
+  depends_on "dbus"
+  depends_on "gnu-sed" => :build
   depends_on "gnutls"
-  depends_on "librsvg" => :recommended
   # Emacs does not support ImageMagick 7:
   # Reported on 2017-03-04: https://debbugs.gnu.org/cgi/bugreport.cgi?bug=25967
   depends_on "imagemagick@6" => :recommended
+  depends_on "librsvg" => :recommended
+  depends_on "little-cms2"
   depends_on "mailutils" => :optional
-
-  if build.with? "x11"
-    depends_on "freetype" => :recommended
-    depends_on "fontconfig" => :recommended
-  end
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
 
   conflicts_with "emacs", :because => "they install conflicting executables"
 
@@ -64,15 +47,11 @@ class EmacsDcp < Formula
       --enable-locallisppath=#{HOMEBREW_PREFIX}/share/emacs/site-lisp
       --infodir=#{info}/emacs
       --prefix=#{prefix}
+      --with-dbus
       --with-gnutls
+      --with-lcms2
       --with-xml2
     ]
-
-    if build.with? "dbus"
-      args << "--with-dbus"
-    else
-      args << "--without-dbus"
-    end
 
     # Note that if ./configure is passed --with-imagemagick but can't find the
     # library it does not fail but imagemagick support will not be available.
@@ -87,12 +66,8 @@ class EmacsDcp < Formula
     args << "--with-rsvg" if build.with? "librsvg"
     args << "--without-pop" if build.with? "mailutils"
 
-    if build.head?
-      args << "--with-lcms2"
-
-      ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin"
-      system "./autogen.sh"
-    end
+    ENV.prepend_path "PATH", Formula["gnu-sed"].opt_libexec/"gnubin"
+    system "./autogen.sh"
 
     if build.with? "cocoa"
       args << "--with-ns" << "--disable-ns-self-contained"
@@ -122,16 +97,7 @@ class EmacsDcp < Formula
         exec #{prefix}/Emacs.app/Contents/MacOS/Emacs "$@"
       EOS
     else
-      if build.with? "x11"
-        # These libs are not specified in xft's .pc. See:
-        # https://trac.macports.org/browser/trunk/dports/editors/emacs/Portfile#L74
-        # https://github.com/Homebrew/homebrew/issues/8156
-        ENV.append "LDFLAGS", "-lfreetype -lfontconfig"
-        args << "--with-x"
-        args << "--with-gif=no" << "--with-tiff=no" << "--with-jpeg=no"
-      else
-        args << "--without-x"
-      end
+      args << "--without-x"
       args << "--without-ns"
 
       system "./configure", *args
@@ -147,40 +113,28 @@ class EmacsDcp < Formula
 
   plist_options :manual => "emacs"
 
-  def plist
-    if build.head?
-      disposition = <<-EOS.undent
-        <key>KeepAlive</key>
-        <dict>
-          <key>Crashed</key>
-          <true/>
-          <key>SuccessfulExit</key>
-          <false/>
-        </dict>
-      EOS
-    else
-      disposition = <<-EOS.undent
-        <key>LaunchOnlyOnce</key>
-        <true/>
-      EOS
-    end
-
-    <<-EOS.undent
+  def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
     <dict>
       <key>Label</key>
       <string>#{plist_name}</string>
-    #{disposition}
+      <key>KeepAlive</key>
+      <dict>
+        <key>Crashed</key>
+        <true/>
+        <key>SuccessfulExit</key>
+        <false/>
+      </dict>
       <key>ProgramArguments</key>
       <array>
         <string>#{opt_bin}/emacs</string>
-        <string>#{build.head? ? "--fg-daemon" : "--daemon"}</string>
+        <string>--fg-daemon</string>
       </array>
     </dict>
     </plist>
-    EOS
+  EOS
   end
 
   test do
