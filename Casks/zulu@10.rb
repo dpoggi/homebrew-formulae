cf51dfd0858556a3e14ca9333f59cd3fb4d3b851
Dan Poggi <dan.poggi@gmail.com>
2018-08-17 12:51:17 -0400

    zulu@10 10.3+5,10.0.2

--- Casks/zulu@10.rb
+++ Casks/zulu@10.rb
@@ -1,9 +1,9 @@
 cask 'zulu@10' do
-  version '10.1+11'
-  sha256 '8888d8260cab86cba06c633d39a03b78982caff3d067c6e5bce7d28e03209b05'
+  version '10.3+5,10.0.2'
+  sha256 'dfc5a9cae3fbfb45d565bcd067829b7273704c976ff00a63794c9f9742fe4f76'
 
   # cdn.azul.com was verified as official when first introduced to the cask
-  url "https://cdn.azul.com/zulu/bin/zulu#{version}-jdk#{version.major}-macosx_x64.dmg",
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-jdk#{version.after_comma}-macosx_x64.dmg",
       referer: 'https://zulu.org/download/?platform=MacOS'
   name 'Zulu Java SE 10 Development Kit'
   homepage 'https://zulu.org/'
@@ -12,13 +12,13 @@ cask 'zulu@10' do
 
   postflight do
     system_command '/bin/mv',
-                   args: ['-f', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version}.jdk"],
+                   args: ['-f', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk"],
                    sudo: true
     system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string BundledApp', "/Library/Java/JavaVirtualMachines/zulu-#{version}.jdk/Contents/Info.plist"],
+                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string BundledApp', "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk/Contents/Info.plist"],
                    sudo: true
     system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu-#{version}.jdk/Contents/Info.plist"],
+                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk/Contents/Info.plist"],
                    sudo: true
   end
 
@@ -26,6 +26,6 @@ cask 'zulu@10' do
                        "com.azulsystems.zulu.#{version.major}",
                      ],
             delete:  [
-                       "/Library/Java/JavaVirtualMachines/zulu-#{version}.jdk",
+                       "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk",
                      ]
 end
