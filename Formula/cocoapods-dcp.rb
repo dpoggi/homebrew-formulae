93032dc5196954d6a01c7334813e8de0caf5eb39
Dan Poggi <dan.poggi@gmail.com>
2019-03-07 12:59:31 -0500

    cocoapods-dcp 1.6.1

--- Formula/cocoapods-dcp.rb
+++ Formula/cocoapods-dcp.rb
@@ -1,13 +1,12 @@
 class CocoapodsDcp < Formula
   desc "Dependency manager for Cocoa projects, with preinstalled plugins"
   homepage "https://cocoapods.org/"
-  url "https://github.com/CocoaPods/CocoaPods/archive/1.5.3.tar.gz"
-  sha256 "04593483efe1279c93cfc2bf25866a6e1a3d0c49c0c10602b060611c1e8b5e20"
-  revision 1
+  url "https://github.com/CocoaPods/CocoaPods/archive/1.6.1.tar.gz"
+  sha256 "482fbb5c89d1c7c4456f9c1aba3b6ee41cfe74f8ea389a4d3a0b0415d30cda40"
 
   devel do
-    url "https://github.com/CocoaPods/CocoaPods/archive/1.6.0.beta.1.tar.gz"
-    sha256 "6c9704f7e4be0903b57039064eebe445016d5acd5060022731d7c90556920195"
+    url "https://github.com/CocoaPods/CocoaPods/archive/1.7.0.beta.1.tar.gz"
+    sha256 "d15c177539d92e9dcb96ca94c48c29337e149e010e34a3012e79a310e802fe0e"
   end
 
   depends_on "ruby" if MacOS.version <= :sierra
@@ -17,14 +16,17 @@ class CocoapodsDcp < Formula
   def install
     ENV["GEM_HOME"] = libexec
 
-    system "gem", "build", "cocoapods.gemspec"
-    system "gem", "install", "cocoapods-#{version}.gem"
+    system "gem", "build", "--norc", "cocoapods.gemspec"
+    system "gem", "install", "--norc", "cocoapods-#{version}.gem"
 
-    system "gem", "install", "cocoapods-dependencies", "-v", "1.3.0"
-    system "gem", "install", "cocoapods-docs", "-v", "0.2.0"
-    system "gem", "install", "cocoapods-rome", "-v", "1.0.0"
+    system "gem", "install", "--norc", "cocoapods-art", "--version", "1.0.2"
+    system "gem", "install", "--norc", "cocoapods-dependencies", "--version", "1.3.0"
+    system "gem", "install", "--norc", "cocoapods-docs", "--version", "0.2.0"
+    system "gem", "install", "--norc", "cocoapods-keys", "--version", "2.1.0"
+    system "gem", "install", "--norc", "cocoapods-packager", "--version", "1.5.0"
+    system "gem", "install", "--norc", "cocoapods-rome", "--version", "1.0.1"
 
-    bin.install libexec/"bin/pod", libexec/"bin/sandbox-pod", libexec/"bin/xcodeproj"
+    bin.install libexec/"bin/pod", libexec/"bin/xcodeproj"
 
     bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"])
   end
