ffa87da74e7dfe23e4dd29651f1ca40da100be43
Dan Poggi <dan.poggi@gmail.com>
2019-03-08 13:58:55 -0500

    cocoapods-dcp 1.5.3_4

--- Formula/cocoapods-dcp.rb
+++ Formula/cocoapods-dcp.rb
@@ -3,7 +3,7 @@ class CocoapodsDcp < Formula
   homepage "https://cocoapods.org/"
   url "https://github.com/CocoaPods/CocoaPods/archive/1.5.3.tar.gz"
   sha256 "04593483efe1279c93cfc2bf25866a6e1a3d0c49c0c10602b060611c1e8b5e20"
-  revision 3
+  revision 4
 
   devel do
     url "https://github.com/CocoaPods/CocoaPods/archive/1.7.0.beta.1.tar.gz"
@@ -27,9 +27,13 @@ class CocoapodsDcp < Formula
     system "gem", "install", "cocoapods-packager", "-v", "1.5.0"
     system "gem", "install", "cocoapods-rome", "-v", "1.0.1"
 
+    inreplace libexec/"gems/RubyInline-3.12.4/lib/inline.rb",
+              "RUBY_PLATFORM =~ /mingw/",
+              "RUBY_PLATFORM =~ /mingw/; cmd = cmd.sub(/^xcrun /, 'xcrun --sdk macosx ') if RUBY_PLATFORM =~ /darwin/"
+
     bin.install libexec/"bin/pod", libexec/"bin/xcodeproj"
 
-    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"], :INLINEDIR => "#{prefix}/var/cache")
+    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"], :INLINEDIR => "#{HOMEBREW_PREFIX}/var/cache")
   end
 
   test do
