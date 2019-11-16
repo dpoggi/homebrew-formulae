de5e573d32036ba182337b30f25e96c3c13d4763
Dan Poggi <dan.poggi@gmail.com>
2019-11-15 20:43:44 -0500

    cocoapods-dcp 1.8.4

--- Formula/cocoapods-dcp.rb
+++ Formula/cocoapods-dcp.rb
@@ -1,8 +1,8 @@
 class CocoapodsDcp < Formula
   desc "Dependency manager for Cocoa projects, with preinstalled plugins"
   homepage "https://cocoapods.org/"
-  url "https://github.com/CocoaPods/CocoaPods/archive/1.7.5.tar.gz"
-  sha256 "508e5f7a7566b3d05ec4e27417dc0a60bedc8e72618b31cb56713034e71337b9"
+  url "https://github.com/CocoaPods/CocoaPods/archive/1.8.4.tar.gz"
+  sha256 "7afe0a8f0d1a83d23a3a04c195229c9bec37d114e6b81b41458e65e33138f8c6"
 
   depends_on "ruby" if MacOS.version <= :sierra
 
@@ -14,17 +14,16 @@ class CocoapodsDcp < Formula
     system "gem", "build", "cocoapods.gemspec"
     system "gem", "install", "cocoapods-#{version}.gem"
 
-    system "gem", "install", "cocoapods-art", "-v", "1.0.3"
+    system "gem", "install", "cocoapods-art", "-v", "1.0.4"
     system "gem", "install", "cocoapods-dependencies", "-v", "1.3.0"
     system "gem", "install", "cocoapods-docs", "-v", "0.2.0"
     system "gem", "install", "cocoapods-keys", "-v", "2.1.0"
-    system "gem", "install", "cocoapods-packager", "-v", "1.5.0"
     system "gem", "install", "cocoapods-rome", "-v", "1.0.1"
 
-    inreplace libexec/"gems/RubyInline-3.12.4/lib/inline.rb",
+    inreplace libexec/"gems/RubyInline-3.12.5/lib/inline.rb",
               "if recompile then",
               "if recompile then\n          RbConfig::CONFIG['srcdir'] = RbConfig::CONFIG['includedir']"
-    inreplace libexec/"gems/RubyInline-3.12.4/lib/inline.rb",
+    inreplace libexec/"gems/RubyInline-3.12.5/lib/inline.rb",
               "RUBY_PLATFORM =~ /mingw/",
               "RUBY_PLATFORM =~ /mingw/\n          cmd = cmd.sub(/^xcrun /, 'xcrun --sdk macosx ') if RUBY_PLATFORM =~ /darwin/"
 
@@ -34,6 +33,6 @@ class CocoapodsDcp < Formula
   end
 
   test do
-    system "#{bin}/pod", "list"
+    assert_match version.to_s, shell_output("#{bin}/pod --version 2>/dev/null")
   end
 end
