2df0f34b075cd8a625aa9f3bffcfbabfb355be8f
Dan Poggi <dan.poggi@gmail.com>
2019-11-15 22:49:23 -0500

    zulu@8 1.8.0_232,8.42.0.23_1

--- Casks/zulu@8.rb
+++ Casks/zulu@8.rb
@@ -1,6 +1,6 @@
 cask 'zulu@8' do
-  version '1.8.0_232,8.42.0.21'
-  sha256 'b726c51d89f6c2baf2a3b1aa847883f98430fd932d875d04394d0e6bd711ce82'
+  version '1.8.0_232,8.42.0.23_1'
+  sha256 '47e5ba7f0d92f85c4273ac4eeb638d9971d75d07ac90cb7a64d7e7567b01d159'
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-ca-jdk#{version.minor}.#{version.patch}.#{version.before_comma.sub(%r{^.*_}, '')}-macosx_x64.dmg",
       referer: 'https://www.azul.com/downloads/zulu-community/'
