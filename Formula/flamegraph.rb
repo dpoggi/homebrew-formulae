cd34642ef62b27e29d2d99aa43a0f1b6e509fefa
Dan Poggi <dan.poggi@gmail.com>
2020-07-02 22:55:31 -0400

    Clean up flamegraph formula

--- Formula/flamegraph.rb
+++ Formula/flamegraph.rb
@@ -3,14 +3,16 @@ class Flamegraph < Formula
   homepage "http://www.brendangregg.com/flamegraphs.html"
   url "https://github.com/brendangregg/FlameGraph/archive/v1.0.tar.gz"
   sha256 "c5ba824228a4f7781336477015cb3b2d8178ffd86bccd5f51864ed52a5ad6675"
+  head "https://github.com/brendangregg/FlameGraph.git"
 
   bottle :unneeded
 
-  depends_on "perl" => :optional
+  depends_on "perl" if MacOS.version > :catalina
 
   def install
     libexec.install Dir["*"]
-    %w[
+
+    executables = %w[
       aix-perf.pl
       difffolded.pl
       files.pl
@@ -18,6 +20,7 @@ class Flamegraph < Formula
       jmaps
       pkgsplit-perf.pl
       range-perf.pl
+      stackcollapse.pl
       stackcollapse-aix.pl
       stackcollapse-elfutils.pl
       stackcollapse-gdb.pl
@@ -25,16 +28,17 @@ class Flamegraph < Formula
       stackcollapse-instruments.pl
       stackcollapse-jstack.pl
       stackcollapse-ljp.awk
-      stackcollapse-perf-sched.awk
       stackcollapse-perf.pl
+      stackcollapse-perf-sched.awk
       stackcollapse-pmc.pl
       stackcollapse-recursive.pl
       stackcollapse-sample.awk
       stackcollapse-stap.pl
       stackcollapse-vsprof.pl
-      stackcollapse.pl
-    ].map { |f| libexec/f }.each do |f|
-      bin.install_symlink f => f.stem
-    end
+    ]
+
+    executables
+      .map { |f| libexec/f }
+      .each { |f| bin.install_symlink f => f.stem }
   end
 end
