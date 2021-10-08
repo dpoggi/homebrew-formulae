e2b85afbf7b1cbf16a606df7a4268336fe46be73
Dan Poggi <dan.poggi@gmail.com>
2021-10-08 17:04:17 -0400

    zulu@8 1.8.0_302,8.56.0.21-ca

--- Casks/zulu@8.rb
+++ Casks/zulu@8.rb
@@ -1,32 +1,26 @@
 cask 'zulu@8' do
-  version '1.8.0_252,8.46.0.19'
-  sha256 '1fa97725adf5df15a84d835156d4ca35433af0d6b0a1aa724336152d4325b827'
+  version "1.8.0_302,8.56.0.21-ca"
+  sha256 "4e667ac2b946d8890a0d9ffe2168e1ad1ccb3f82c3683ccd3f10ce5349c874a7"
 
-  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-ca-jdk#{version.minor}.#{version.patch.sub(%r{_}, '.')}-macosx_x64.dmg",
-      referer: 'https://www.azul.com/downloads/zulu-community/'
-  name 'Azul Zulu OpenJDK 8'
-  homepage 'https://www.azul.com/products/zulu-community/'
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma[2...].sub("_", ".")}-macosx_x64.dmg",
+      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+  name "Azul Zulu OpenJDK 8"
+  desc "Java SE 8 development kit from Azul Systems"
+  homepage "https://www.azul.com/products/core/"
 
-  conflicts_with cask: 'zulu8'
+  conflicts_with cask: ["adoptopenjdk8", "zulu8"]
 
-  pkg "Double-Click to Install Zulu #{version.minor}.pkg"
+  pkg "Double-Click to Install Azul Zulu JDK #{version.minor}.pkg"
 
   postflight do
-    system_command '/bin/mv',
-                   args: ['-f', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.minor}.jdk", "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk"],
-                   sudo: true
-    system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string BundledApp', "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Info.plist"],
-                   sudo: true
-    system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk/Contents/Info.plist"],
+    system_command "/bin/mv",
+                   args: ["-f",
+                          "--",
+                          "/Library/Java/JavaVirtualMachines/zulu-#{version.minor}.jdk",
+                          "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk"],
                    sudo: true
   end
 
-  uninstall pkgutil: [
-                       "com.azulsystems.zulu.#{version.minor}",
-                     ],
-            delete:  [
-                       "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk",
-                     ]
+  uninstall pkgutil: "com.azulsystems.zulu.#{version.minor}",
+            delete:  "/Library/Java/JavaVirtualMachines/zulu#{version.before_comma}.jdk"
 end
